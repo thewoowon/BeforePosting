a1d3f52fa6e45899755f7b71b5c029c58f1f7608
thewoowon <thewoowon@naver.com>
2021-08-04 15:57:34 +0900

    프로그래머스 프린터

---
+++ Printer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] p = {1,1,9,1,1,1};
+            int l = 0;
+
+            int answer = solution.solution(p, l);
+
+            Console.WriteLine(answer);
+        }
+
+        public partial class Solution
+        {
+            public int solution(int[] priorities, int location)
+            {
+
+                Queue<int> prior = new Queue<int>(priorities);
+                Queue<int> indexQueue = new Queue<int>();
+
+                for (int i = 0; i < priorities.Length; i++)
+                {
+                    indexQueue.Enqueue(i); //인덱스를 저장하는 큐 초기화
+                }
+
+
+                while (true)
+                {
+                    int sw = 1;
+                    int onStagePriority = prior.Dequeue();
+                    int onStageIndex = indexQueue.Dequeue();
+
+                    foreach (var item in prior)
+                    {
+                        if (onStagePriority < item)
+                        {
+                            sw = 0;
+                            indexQueue.Enqueue(onStageIndex);
+                            prior.Enqueue(onStagePriority);
+                            break;
+                        }
+                    }
+
+                    if (sw != 0)
+                    {
+                        if (location == onStageIndex)
+                        {
+                            return priorities.Length - prior.Count;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
