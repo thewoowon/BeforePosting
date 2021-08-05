c40dd9a4d761cacfc34756f26292c91d472b3895
thewoowon <thewoowon@naver.com>
2021-08-05 16:34:27 +0900

    프로그래머스 주식가격

---
+++ StockPrice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] temp = {1, 2, 3, 2, 3 };
+
+            solution.solution(temp);
+        }
+
+        public partial class Solution
+        {
+            public int[] solution(int[] prices)
+            {
+                int[] answer = new int[prices.Length];
+
+
+                for (int i = 0; i < prices.Length; i++)
+                {
+                    int baseValue = prices[i];
+                    int count = 0;
+
+                    for (int j = i+1; j < prices.Length; j++)
+                    {
+                        count++;
+                        if (prices[j]< baseValue)
+                        {
+                            break;
+                        }
+                    }
+                    answer[i] = count;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
