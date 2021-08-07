98da1e032003cf0f5148e8ba01f4db01568f85da
thewoowon <thewoowon@naver.com>
2021-08-07 11:28:47 +0900

    프로그래머스 가장 큰수

---
+++ K/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace K
+{
+ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] a = { 1, 5, 2, 6, 3, 7, 4 };
+            int[,] b = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+
+            solution.solution(a,b);
+            
+        }
+
+        public partial class Solution
+        {
+            public int[] solution(int[] array, int[,] commands)
+            {
+                int count = commands.GetLength(0);
+                int[] answer = new int[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    var temp = new ArraySegment<int>(array, commands[i,0]-1, commands[i,1] - commands[i,0] + 1);
+                    List<int> tempList = new List<int>(temp.ToArray());
+
+                    tempList.Sort();
+
+                    answer[i] = tempList[commands[i, 2] - 1];
+                }
+
+                return answer;
+            }
+        }
+    }
+}
---
+++ Max/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public partial class Solution
+        {
+            public string solution(int[] numbers)
+            {
+                string answer = "";
+
+                
+                StringBuilder sb = new StringBuilder();
+
+                
+                Array.Sort(numbers, (x, y) =>
+                string.Compare(y.ToString() + x.ToString(), x.ToString() + y.ToString()));
+
+                
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sb.Append(numbers[i].ToString());
+                }
+
+                
+                answer = sb.ToString();
+
+               
+                int count = 0;
+
+                
+                foreach (var zero in answer)
+                {
+                    if (zero == '0')
+                        count++;
+                }
+
+                
+                if (count == numbers.Length)
+                    return "0";
+
+                
+                else
+                    return answer;
+            }
+        }
+    }
+}
